8e7d1a3d1e058e25bb84ce0072f4542d1338e9d6
Xavi Torelló <info@xaviertorello.cat>
2022-10-21 13:09:10 +0200

    Init app

--- cryptos/test/test_function.py
+++ cryptos/test/test_function.py
@@ -1,18 +1,19 @@
 import pytest
-from ..app import Flask
+from ..app import app
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tests.db"
 
 # Test GET in homepage
 def test_homepage():
-    flask_app = Flask("flask_test.cfg")
-    with flask_app.test_client() as test_client:
+    with app.test_client() as test_client:
         response = test_client.get("/")
         assert response.status_code == 200
         assert b"Crypto Tracker" in response.data
 
 # Test POST in add page
 def test_post_add_page():
-    flask_app = Flask("flask_test.cfg")
-    with flask_app.test_client() as test_client:
+    with app.test_client() as test_client:
         response = test_client.post("/ajouter")
         assert response.status_code == 405
         assert b"Ajouter" not in response.data
